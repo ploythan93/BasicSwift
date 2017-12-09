e5352c14a0f85a8ec2e4403c12e7a52b4f737387
ploythan <ploythan93@gmail.com>
2017-12-09 11:37:44 +0700

    ค่าคงที่

--- Contents.swift
+++ Contents.swift
@@ -16,6 +16,21 @@ let conNameString = "Nobita"
 // conNameString = "doremon" จะทำแล้ว error เพราะค่าคงที่เปลี่ยนค่าไม่ได้
 // วิธีการแก้ คือ เปลี่ยน let เป็น var
 
+//การประกาศตัวแปรแบบกำหนด data type
+var surname: String = "Trakarn"
+var myNumber: Int = 321
+var num: Double = 123.456
+var myStatus: Bool = true
+
+
+
+
+
+
+
+
+
+
 
 
 
