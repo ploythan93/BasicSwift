715a96d299604377c29c795024a0ff4f06947254
ploythan <ploythan93@gmail.com>
2017-12-09 11:01:07 +0700

    test

---
+++ Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+// cmd+b = compile
+import UIKit //libraly name UIKit
+
+//การประกาศตัวแปรแบบแก้ไขได้
+var nameString = "ploythan" //ประกาศแบบ string ที่ใช้ค่ามรการกำหนด data type
+var numberInt = 123 // ประกาศเป็น int
+var numDouble = 12.34 //เลขที่เศษ
+var statusBo = true //ประกาศแบบ boolean
+nameString = "ABCD" //ค่าจะเปลี่ยนได้
+numberInt = 321
+
+
+
+
+
+
